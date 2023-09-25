ad790943ae7293e4621a9fe19e9f4da44c2c286f
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-09-25 07:24:21 +0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # playing-now-spotify
- 
+
+![Alt text](<screenshots/v1.0.0/Screenshot 2023-09-25 071530.jpg>)![Alt text](<screenshots/v1.0.0/Screenshot 2023-09-25 071721.jpg>)![Alt text](<screenshots/v1.0.0/Screenshot 2023-09-25 072232.jpg>)
---
+++ src/renderer.ts
@@ -0,0 +1,9 @@
+const contaner = document.querySelector("#playing-now");
+// function playingRightNow() {
+//   contaner.innerHTML = `<img src="https://spotify-github-profile.vercel.app/api/view?uid=31pdpcquhjbfdyqmhp6j6sy3xaxq&cover_image=true&theme=default&show_offline=true&background_color=121212&bar_color_cover=true" width="300" />`;
+//   console.log("first");
+// }
+
+// playingRightNow();
+
+contaner.addEventListener("click", location.reload);
